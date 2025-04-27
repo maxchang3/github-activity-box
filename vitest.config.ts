9ee57f514ba7a16dcd9b1ceff8fda9997613da89
MaxChang3 <36927158+maxchang3@users.noreply.github.com>
2025-04-28 01:06:00 +0800

    chore: remove test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,10 +4,6 @@ export default defineConfig({
     test: {
         globals: true,
         setupFiles: ['./test/setup.ts'],
-        coverage: {
-            provider: 'v8',
-            reporter: ['text', 'lcov', 'clover', 'json'],
-        },
         typecheck: {
             tsconfig: './tsconfig.vitest.json',
         },

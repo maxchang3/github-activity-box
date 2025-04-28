e0765045b7f5008ec427b43efd8951ceb3804e28
MaxChang3 <36927158+maxchang3@users.noreply.github.com>
2025-04-28 20:43:16 +0800

    refactor: restructure search issues logic

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,6 +1,6 @@
 import SearchIssues from '@/SearchIssues.graphql'
 import { env } from '@/env'
-import { SearchResponseSchema } from '@/schema'
+import { type SearchResponse, SearchResponseSchema } from '@/schema'
 import { graphql } from '@octokit/graphql'
 
 const graphqlWithAuth = graphql.defaults({
@@ -9,18 +9,20 @@ const graphqlWithAuth = graphql.defaults({
     },
 })
 
-export const getIssues = async (author: string) => {
-    return SearchResponseSchema.parse(
-        await graphqlWithAuth(SearchIssues, {
-            searchQuery: `author:${author} is:issue sort:updated-desc`,
-        })
-    )
+type IssueType = 'issue' | 'pr'
+
+/**
+ * Executes a search query via GitHub's GraphQL API
+ *
+ * *Pull requests are a type of issue.* —— About pull requests, GitHub Docs
+ * @refer https://docs.github.com/en/graphql/reference/objects#searchresultitemconnection
+ */
+const searchIssues = async (author: string, type: IssueType): Promise<SearchResponse> => {
+    const response = await graphqlWithAuth(SearchIssues, {
+        searchQuery: `author:${author} is:${type} sort:updated-desc`,
+    })
+    return SearchResponseSchema.parse(response)
 }
 
-export const getPRs = async (author: string) => {
-    return SearchResponseSchema.parse(
-        await graphqlWithAuth(SearchIssues, {
-            searchQuery: `author:${author} is:pr sort:updated-desc`,
-        })
-    )
-}
+export const getIssues = (author: string) => searchIssues(author, 'issue')
+export const getPRs = (author: string) => searchIssues(author, 'pr')

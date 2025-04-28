e3a74043126031b9786d39f22bac5bcd2148c4b5
MaxChang3 <36927158+maxchang3@users.noreply.github.com>
2025-04-28 12:49:08 +0800

    refactor:  better alias, remove vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,15 @@
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
         globals: true,
         setupFiles: ['./test/setup.ts'],
-        typecheck: {
-            tsconfig: './tsconfig.vitest.json',
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('src/', import.meta.url)),
+            '~': fileURLToPath(new URL('test/', import.meta.url)),
         },
     },
 })
